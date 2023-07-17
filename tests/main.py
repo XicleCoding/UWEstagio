ad244eecad21bcac98e22574785bfa43c6fdd9f4
FranciscoMFR <franciscor@ua.pt>
2023-07-17 16:48:58 +0100

    skip-gram and cbow

--- tests/main.py
+++ tests/main.py
@@ -5,7 +5,9 @@ import word2vecTesting as w2vTst
 
 
 if __name__ == '__main__':
-    tokenTst.token()
-    tagTst.tag()
-    tagTst.tag2()
-    w2vTst.word2vec()
+    #tokenTst.token()
+    #tagTst.tag()
+    #tagTst.tag2()
+    #w2vTst.word2vec()
+    w2vTst.word2vecsg()
+    w2vTst.word2veccbow()
--- tests/word2vecTesting.py
+++ tests/word2vecTesting.py
@@ -4,7 +4,6 @@ import nltk
 
 
 def word2vec():
-    nltk.download('punkt')
 
     # Corpus of Portuguese text
     corpus = [
@@ -20,6 +19,60 @@ def word2vec():
 
     # Training the Word2Vec model
     model = Word2Vec(tokenized_corpus, vector_size=100, window=5, min_count=1, workers=4)
+
+    # Accessing word vectors
+    word_vectors = model.wv
+
+    # Example usage: Finding most similar words
+    similar_words = word_vectors.most_similar("gato", topn=3)
+    print(similar_words)
+    return
+
+
+def word2vecsg():
+        
+    # Corpus of Portuguese text
+    corpus = [
+        "O gato é preto",
+        "O cachorro é marrom",
+        "O céu está azul",
+        "A grama é verde",
+        "Eu gosto de sorvete"
+    ]
+
+    # Preprocessing the text and tokenizing
+    tokenized_corpus = [nltk.word_tokenize(sentence.lower()) for sentence in corpus]
+
+    # Training the Word2Vec model with skip-gram architecture
+    model = Word2Vec(tokenized_corpus, vector_size=100, window=5, min_count=1, sg=1, workers=4)
+
+    # Accessing word vectors
+    word_vectors = model.wv
+
+    # Example usage: Finding most similar words
+    similar_words = word_vectors.most_similar("gato", topn=3)
+    print(similar_words)
+
+    return
+
+
+
+def word2veccbow():
+
+    # Corpus of Portuguese text
+    corpus = [
+        "O gato é preto",
+        "O cachorro é marrom",
+        "O céu está azul",
+        "A grama é verde",
+        "Eu gosto de sorvete"
+    ]
+
+    # Preprocessing the text and tokenizing
+    tokenized_corpus = [nltk.word_tokenize(sentence.lower()) for sentence in corpus]
+
+    # Training the Word2Vec model with CBOW architecture
+    model = Word2Vec(tokenized_corpus, vector_size=100, window=5, min_count=1, sg=0, workers=4)
 
     # Accessing word vectors
     word_vectors = model.wv
