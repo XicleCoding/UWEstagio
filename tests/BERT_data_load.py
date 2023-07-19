68bd0d0c396e41061c93f2ce5bba0731c761cc20
FranciscoMFR <franciscor@ua.pt>
2023-07-19 01:43:59 +0100

    Before BERT implement

--- tests/BERT_data_load.py
+++ tests/BERT_data_load.py
@@ -4,22 +4,25 @@ import pandas as pd
 import numpy as np
 from sklearn import metrics, manifold
 
-lst_dics = []
-with open('data.json', mode='r', errors='ignore') as json_file:
-    for dic in json_file:
-        lst_dics.append( json.loads(dic) )
+def load() -> object:
+    lst_dics = []
+    with open('data_news.json', mode='r', errors='ignore') as json_file:
+        for dic in json_file:
+            lst_dics.append( json.loads(dic) )
 
-## print the first one
-print(lst_dics[0])
+    ## print the first one
+    print(lst_dics[0])
 
-## create dtf
-dtf = pd.DataFrame(lst_dics)
+    ## create dtf
+    dtf = pd.DataFrame(lst_dics)
 
-## filter categories
-dtf = dtf[ dtf["category"].isin(['ENTERTAINMENT','POLITICS','TECH'])        ][["category","headline"]]
+    ## filter categories
+    dtf = dtf[ dtf["category"].isin(['ENTERTAINMENT','POLITICS','TECH'])        ][["category","headline"]]
 
-## rename columns
-dtf = dtf.rename(columns={"category":"y", "headline":"text"})
+    ## rename columns
+    dtf = dtf.rename(columns={"category":"y", "headline":"text"})
 
-## print 5 random rows
-print(dtf.sample(5))
+    ## print 5 random rows
+    print(dtf.sample(5))
+    
+    return dtf
--- tests/main.py
+++ tests/main.py
@@ -1,13 +1,45 @@
-import tokenTesting as tokenTst
-import tagTesting as tagTst
-import word2vecTesting as w2vTst
+import BERT_data_load 
+from nlp_utils import *
+import nltk
 
 
 
 if __name__ == '__main__':
-    #tokenTst.token()
-    #tagTst.tag()
-    #tagTst.tag2()
-    #w2vTst.word2vec()
-    w2vTst.word2vecsg()
-    w2vTst.word2veccbow()
+    #Downloads
+    ##nltk.download('stopwords')
+    ##nltk.download('wordnet')
+
+    dtf = BERT_data_load.load()
+
+    #Testing stop words generation
+    lst_stopwords = nltk.corpus.stopwords.words("english")
+    ##print('----------------STOP WORDS----------------\n')
+    ##print(lst_stopwords)
+    
+    #Text preprocessing
+    dtf["text_clean"] = dtf["text"].apply(lambda x:utils_preprocess_text(
+                                                                            x,
+                                                                            lst_regex=None,
+                                                                            punkt=True,
+                                                                            lower=True,
+                                                                            slang=False,
+                                                                            lst_stopwords=lst_stopwords,
+                                                                            stemm=False, 
+                                                                            lemm=True, 
+                                                                        )) #txt, lst_regex=None, punkt=True, lower=True, slang=True, lst_stopwords=None, stemm=False, lemm=True
+    ##print('----------------PREPROCESSED TEXT EXAMPLE----------------\n')
+    ##print(dtf.head())
+
+    #Target Clusters Creation
+    ## Create Dictionary {category:[keywords]}
+    nlp = gensim_api.load("glove-wiki-gigaword-300")
+    dic_clusters = {}
+    dic_clusters["ENTERTAINMENT"] = get_similar_words(['celebrity','cinema','movie','music'], top=30, nlp=nlp)
+    dic_clusters["POLITICS"] = get_similar_words(['gop','clinton','president','obama','republican'], top=30, nlp=nlp)
+    dic_clusters["TECH"] = get_similar_words(['amazon','android','app','apple','facebook','google','tech'], top=30, nlp=nlp)
+    
+    ## print some examples
+    ##for k,v in dic_clusters.items():
+    ##    print(f'{k} : {v[0:5]} ... {len(v)}')
+
+    plot_w2v_cluster(dic_clusters, nlp, plot_type="2d", annotate=True, figsize=(20,10))
